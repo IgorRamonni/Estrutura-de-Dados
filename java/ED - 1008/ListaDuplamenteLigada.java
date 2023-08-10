ec9a786fb8156d66eebe31f63dac64218629fd5c
Igor <lima.igormartins@gmail.com>
2023-08-10 18:44:46 -0300

    última atualização da aula 10/08

--- java/ED - 1008/ListaDuplamenteLigada.java
+++ java/ED - 1008/ListaDuplamenteLigada.java
@@ -1,6 +1,6 @@
 public class ListaDuplamenteLigada<T>{
     private int tamanho;
-    protected NoDplo<T> inicio,fim;
+    private NoDuplo<T> inicio,fim;
 
     public ListaDuplamenteLigada(){
         tamanho = 0;
@@ -43,4 +43,54 @@ public class ListaDuplamenteLigada<T>{
         }
         return no.getProximo();
      }
+
+     public void adicionaAntes(NoDuplo<T> noExistente, NoDuplo<T> noNovo) throws IllegalArgumentExceptio{
+        NoDuplo<T> noInserido = getAnterior(noExistente);
+        noNovo.setNoAnterior(noInserido);
+        noNovo.setNoProximo(noExistente);
+        noExistente.setNoAnterior(noNovo);
+        noInserido.setNoProximo(noNovo);
+        this.tamanho++;
+     }
+
+     public void adicionaDepois(NoDuplo<T> noExistente, NoDuplo<T> noNovo) throws IllegalArgumentException{
+        NoDuplo<T> noInserido = getProximo (noExistente);
+        noNovo.setNoAnterior(noExistente);
+        noNovo.setNoProximo(noInserido);
+        noInserido.setNoAnterior(noNovo);
+        noExistente.setNoProximo(noNovo);
+        this.tamanho ++;
+     }
+
+     public void adicionaInicio(NoDuplo<T> no){
+        adicionaDepois(inicio, no);
+     }
+
+     public void adicionaFim(NoDuplo<T> no){
+        adicionaAntes(fim, no);
+     }
+
+     public vod remove(NoDuplo<T> noExistente) throws
+     IllegalArgumentException{
+        NoDuplo<T> NoAnterior = getAnterior(noExistente);
+        NoDuplo<T> NoPosterior=  getProximo(noExistent);
+        noAnterior.setNoProximo(noProximo);
+        noProximo.setNoAnterior(noAnterior);
+        noExistente.setNoAnterior(null);
+        noExistente.setNoProximo(null);
+        tamanho--;
+     }
+    public String toString(){
+        String retorno = "[";
+        NoDuplo<T> no = inicio.getNoProximo();
+        while(no != fim){
+            retorno += no.toString();
+            no = no.getNoProximo();
+            if(!no == fim ){    
+                retorno += ",";
+        }
+    }
+        retorno += "]";
+        return retorno;
+    }
 }
--- java/ED - 1008/Main.java
+++ java/ED - 1008/Main.java
@@ -1,8 +1,11 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("Oi mundo");
-        NoDuplo<String> no = new NoDuplo("A", null,null);
-        NoDuplo<String> no2 = new NoDuplo("B",no,null);
-        no.setNoAnterior;
+        ListaDuplamenteLigada<String> lista = new ListaDuplamenteLigada<String>();
+        lista.adicionaInicio(new NoDuplo<String>("A",null,null));
+        lista.adicionaInicio(new NoDuplo<String>("B",null,null));
+        lista.adicionaInicio(new NoDuplo<String>("C",null,null));
+        System.out.println(lista);
+        lista.adicionaInicio(new NoDuplo<String>("Ã",null,null));
     }
 }
