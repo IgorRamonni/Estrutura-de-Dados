57106eb31e8c78c8b9bbb4e0ec512bb0c0a2050e
Igor <lima.igormartins@gmail.com>
2023-08-10 16:29:18 -0300

    Salvando dados

--- java/ED - 1008/Main.java
+++ java/ED - 1008/Main.java
@@ -1,5 +1,8 @@
 public class main{
     public static void main(String[] args) {
         System.out.println("Oi mundo");
+        NoDuplo<String> no = new NoDuplo("A", null,null);
+        NoDuplo<String> no2 = new NoDuplo("B",no,null);
+        no.setNoAnterior;
     }
 }
--- java/ED - 1008/NoDuplo.java
+++ java/ED - 1008/NoDuplo.java
@@ -1,4 +1,47 @@
 public class NoDuplo {
-    protected NoDuplo<T> noANterior;
+    protected NoDuplo<T> noAnterior;
     protected NoDuplo<T> noProximo;
+    protected T objeto;
+
+    public NoDuplo<T>(T elemento, NoDuplo<T>anterior, NoDuplo<T> proximo){
+        this.elemento = elemento;
+        this.noAnterior = anterior;
+        this.noProximo = proximo;
+    }
+    public NoDuplo<T> getNoAnterior() {
+        return noAnterior;
+    }
+    public void setNoAnterior(NoDuplo<T> noAnterior) {
+        this.noAnterior = noAnterior;
+    }
+    public NoDuplo<T> getNoProximo() {
+        return noProximo;
+    }
+    public void setNoProximo(NoDuplo<T> noProximo) {
+        this.noProximo = noProximo;
+    }
+    public T getObjeto() {
+        return objeto;
+    }
+    public void setObjeto(T objeto) {
+        this.objeto = objeto;
+    }
+    public T getElemento() {
+        return elemento;
+    }
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+    public T getNoDuplo() {
+        return NoDuplo;
+    }
+    public void setNoDuplo(T noDuplo) {
+        NoDuplo = noDuplo;
+    }
+    public NoDuplo<T> getProximo() {
+        return proximo;
+    }
+    public void setProximo(NoDuplo<T> proximo) {
+        this.proximo = proximo;
+    }
 }
